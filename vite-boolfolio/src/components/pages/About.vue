c8a02aa607fbd6a2351185370adb5e576bdba9b7
fabiomarzano <fabiomarzano1993@gmail.com>
2024-10-20 17:27:25 +0200

    [UPD] add-about-component

---
+++ vite-boolfolio/src/components/pages/About.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="page-about">
+      <h1>About Me</h1>
+      <p>Qui trovi la pagina "About".</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .page-about {
+    padding: 20px;
+    text-align: center;
+  }
+  </style>
