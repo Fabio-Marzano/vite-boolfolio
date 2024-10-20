c766715cbe7cdf684954a967c053425e7672e4d0
fabiomarzano <fabiomarzano1993@gmail.com>
2024-10-20 17:18:10 +0200

    [UPD] add-components-home

---
+++ vite-boolfolio/src/components/pages/Home.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    
+}
+</script>
+<template >
+    <div class="main py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center mb-4">Benvenuti sul sito di sviluppo di Fabio</h1>
+                    <p class="text-center lead">
+                        Sito contenenente i progetti realizzati</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+<style >
+    
+</style>
