8dea504e9bec4bc412c8e3a43b344e19afc0120e
fabiomarzano <fabiomarzano1993@gmail.com>
2024-10-22 18:22:43 +0200

    [UPD] add-router-js

---
+++ vite-boolfolio/src/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    props:{
+        menu: Array
+    }
+}
+</script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center py-3">
+                    <ul class="nav-list">
+                        <li v-for="(item, index) in menu" :key="index">
+                          <router-link :to="{ name: item.name }">{{ item.label }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style lang="">
+    
+</style>
+</script>
+
+<template>
+<AppHeader :menu="store.menu" />
+<router-view></router-view>
+
+
+</template>
+
+<style lang="scss">
+@import './styles/generals.scss';
+</style>
---
+++ vite-boolfolio/src/styles/router.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import PageHome from './pages/PageHome.vue';
+import PagePortfolio from './pages/PagePortfolio.vue';
+import PageAbout from './pages/PageAbout.vue';
+import PageSingleProject from './pages/PageSingleProject.vue';
+import PageNotFound from './pages/PageNotFound.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'homepage',
+            component: PageHome
+        },
+        {
+            path: '/portfolio',
+            name: 'portfolio',
+            component: PagePortfolio
+        },
+        {
+            path: '/portfolio/:slug',
+            name: 'single-project',
+            component: PageSingleProject
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: PageAbout
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: PageNotFound
+        }
+    ]
+});
+
+export { router };
